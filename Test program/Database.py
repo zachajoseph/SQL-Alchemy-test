9404bfa0035e6f67901a5044fe4b0a339cc9bcff
zachajoseph <zachajoseph@gmail.com>
2020-12-20 20:18:19 -0800

    Edit to Example

--- Test program/Database.py
+++ Test program/Database.py
@@ -153,25 +153,3 @@ class Comment(Model):
     def rendered_text(self):
         from flask_website.utils import format_creole
         return format_creole(self.text)
-
-
-class OpenIDAssociation(Model):
-    __tablename__ = 'openid_associations'
-    id = Column('association_id', Integer, primary_key=True)
-    server_url = Column(String(1024))
-    handle = Column(String(255))
-    secret = Column(String(255))
-    issued = Column(Integer)
-    lifetime = Column(Integer)
-    assoc_type = Column(String(64))
-
-
-class OpenIDUserNonce(Model):
-    __tablename__ = 'openid_user_nonces'
-    id = Column('user_nonce_id', Integer, primary_key=True)
-    server_url = Column(String(1024))
-    timestamp = Column(Integer)
-    salt = Column(String(40))
-
-
-event.listen(db_session, 'after_flush', search.update_model_based_indexes)
